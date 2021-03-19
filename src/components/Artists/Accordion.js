4e678acf25fb9fd5706310710104e6037a87ea0b
Luis montoya <luishm2024@gmail.com>
2021-03-18 23:43:35 -0500

    creacion carpeta estilos para accordion

--- src/components/Artists/Accordion.js
+++ src/components/Artists/Accordion.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableWithoutFeedback} from 'react-native';
+import {styles} from '../../styles/Accordion';
 
 export default function Accordion({item}) {
   const [expanded, setExpanded] = React.useState(false);
   return (
-    <View>
+    <View style={styles.containerAccordion}>
       <Text></Text>
     </View>
   );
