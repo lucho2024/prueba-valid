3321f58a5822e8b08c52c2e9f64b7a5f9851d8c7
Luis montoya <luishm2024@gmail.com>
2021-03-19 11:07:43 -0500

    cambio iconos navegacion

--- src/nav/BottomNav.js
+++ src/nav/BottomNav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import TopArtists from '../screens/TopArtists';
 import TopTracks from '../screens/TopTracks';
+import Icons from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator();
 export default function BottomNav() {
@@ -12,6 +13,9 @@ export default function BottomNav() {
         component={TopArtists}
         options={{
           tabBarLabel: 'Top Artist',
+          tabBarIcon: ({color, size}) => (
+            <Icons name="users" color={'black'} size={26} />
+          ),
         }}
       />
       <Tab.Screen
@@ -19,6 +23,9 @@ export default function BottomNav() {
         component={TopTracks}
         options={{
           tabBarLabel: 'Top Tracks',
+          tabBarIcon: ({color, size}) => (
+            <Icons name="music" color={'black'} size={26} />
+          ),
         }}
       />
     </Tab.Navigator>
