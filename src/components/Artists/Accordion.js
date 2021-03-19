751617c304d71cf61ec749b05d17076d8560b122
Luis montoya <luishm2024@gmail.com>
2021-03-19 00:37:22 -0500

    mostrando todos los detalles y la imagen

--- src/components/Artists/Accordion.js
+++ src/components/Artists/Accordion.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableWithoutFeedback} from 'react-native';
+import {View, Text, TouchableWithoutFeedback, Image} from 'react-native';
 import {styles} from '../../styles/Accordion';
 import Icons from 'react-native-vector-icons/FontAwesome5';
 
@@ -8,8 +8,36 @@ export default function Accordion({item}) {
   return (
     <View style={styles.containerAccordion}>
       <TouchableWithoutFeedback onPress={() => setExpanded(!expanded)}>
-        <View style={styles.headerContainerAccordion}></View>
+        <View style={styles.headerContainerAccordion}>
+          <View style={styles.containerleft}>
+            <Text style={[styles.title, {marginLeft: 5}]}>{item.name}</Text>
+          </View>
+          <View>
+            {expanded ? (
+              <Icons name="chevron-up" color={'black'} size={26} />
+            ) : (
+              <Icons name="chevron-down" color={'black'} size={26} />
+            )}
+          </View>
+        </View>
       </TouchableWithoutFeedback>
+      <View style={expanded ? styles.containerExpand : {display: 'none'}}>
+        <View>
+          <Text>Listeners :</Text>
+          <Text>{item.listeners}</Text>
+        </View>
+        <View>
+          <Text>Mbid :</Text>
+          <Text>{item.mbid}</Text>
+        </View>
+        <View>
+          <Text>Url :</Text>
+          <Text>{item.url}</Text>
+        </View>
+        <View>
+          <Image source={{uri: item.image[2]['#text']}} style={styles.img} />
+        </View>
+      </View>
     </View>
   );
 }
--- src/styles/Accordion.js
+++ src/styles/Accordion.js
@@ -10,4 +10,26 @@ export const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginHorizontal: 5,
   },
+  containerleft: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  containerExpand: {
+    marginHorizontal: 5,
+  },
+  title: {
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 19,
+  },
+  text: {
+    fontSize: 24,
+    color: 'black',
+    marginLeft: 10,
+  },
+  img: {
+    width: 50,
+    height: 50,
+  },
 });
