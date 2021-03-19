df211b4275de298d1a318e776452d2f614b1ea0b
Luis montoya <luishm2024@gmail.com>
2021-03-19 00:23:12 -0500

    instalacion react-native-elements y raect-vector-icons,crecion boton para expandir el acordion y empezando con los estilos

--- src/components/Artists/Accordion.js
+++ src/components/Artists/Accordion.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {View, Text, TouchableWithoutFeedback} from 'react-native';
 import {styles} from '../../styles/Accordion';
+import Icons from 'react-native-vector-icons/FontAwesome5';
 
 export default function Accordion({item}) {
   const [expanded, setExpanded] = React.useState(false);
   return (
     <View style={styles.containerAccordion}>
-      <Text></Text>
+      <TouchableWithoutFeedback onPress={() => setExpanded(!expanded)}>
+        <View style={styles.headerContainerAccordion}></View>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
--- src/styles/Accordion.js
+++ src/styles/Accordion.js
@@ -4,4 +4,10 @@ export const styles = StyleSheet.create({
   containerAccordion: {
     marginVertical: 15,
   },
+  headerContainerAccordion: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 5,
+  },
 });
